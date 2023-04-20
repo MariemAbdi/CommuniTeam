fb0df8ab01d4538cf1d911c7209c667f6ca3c86e
Toumi Mohamed Koussay <toumikoussay@hotmail.fr>
2023-04-20 12:51:36 +0100

    adding a drawer with an avatar and a custom search bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,10 +10,66 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Icon customIcon = const Icon(Icons.search);
+  Widget customSearchBar = const Text('Channel Name');
   final user = FirebaseAuth.instance.currentUser!;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        /*title: Text('CommuniTeam',
+       style: GoogleFonts.robotoCondensed(
+                    color: Colors.white,
+                    fontSize: 20,),
+                    ),*/
+        title: customSearchBar,
+        titleTextStyle:
+            GoogleFonts.robotoCondensed(color: Colors.white, fontSize: 20),
+        backgroundColor: const Color.fromARGB(255, 97, 31, 105),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                if (customIcon.icon == Icons.search) {
+                  customIcon = const Icon(Icons.cancel);
+                  customSearchBar = const ListTile(
+                    leading: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                      size: 28,
+                    ),
+                    title: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'type in channel name...',
+                        hintStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontStyle: FontStyle.italic,
+                        ),
+                        border: InputBorder.none,
+                      ),
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                } else {
+                  customIcon = const Icon(Icons.search);
+                  customSearchBar = Text(
+                    'Channel Name',
+                    style: GoogleFonts.robotoCondensed(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  );
+                }
+              });
+            },
+            icon: customIcon,
+          )
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -25,7 +81,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   FirebaseAuth.instance.signOut();
                 },
                 color: const Color.fromARGB(255, 97, 31, 105),
-                
                 child: Text(
                   'Sign out',
                   style: GoogleFonts.robotoCondensed(
@@ -34,6 +89,56 @@ class _HomeScreenState extends State<HomeScreen> {
                     fontSize: 18,
                   ),
                 ))
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 97, 31, 105),
+              ),
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('images/avatar.png'),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'My Name',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
           ],
         ),
       ),
